de4e6e495a860d197852ef9fa39de8e4de72a9af
Emma CLUZET <emma.cluzet@ens.uvsq.fr>
2023-03-19 19:58:23 +0100

    new

--- TD/TD4/TD04_fonctionnel.py
+++ TD/TD4/TD04_fonctionnel.py
@@ -5,7 +5,6 @@ from PIL import Image
 from PIL import ImageTk 
 from tkinter import filedialog
 from tkinter import simpledialog
-from random import*
 
 def save(matPix, filename):
     Image.fromarray(matPix).save(filename)
@@ -64,6 +63,9 @@ def reaffiche():
         canvas['height'] = imgDebut.height()
         nomImgCourante = nomImgDebut
 
+def arreter():
+    fenetre.quit()
+
 def filtre_vert():
     mat=load(nomImgCourante)
     for i in range(mat.shape[0]):
@@ -121,10 +123,10 @@ def zoom():
 
 def shrink():
     mat=load(nomImgCourante)
-    matshrink=np.empty((mat.shape[0]//2,mat.shape[1]//2,4),dtype=np.uint8)
+    matshrink = np.empty((mat.shape[0]//2, mat.shape[1]//2,3), dtype = np.uint8)
     for i in range(matshrink.shape[0]):
         for j in range(matshrink.shape[1]):
-            matshrink[i,j]=(mat[2*i,2*j]//4+mat[2*i,2*j+1]//4+mat[2*i+1,2*j]//4+mat[2*i+1,2*j+1]//4)
+            matshrink[i,j]=(mat[2*i,2*j]//4+mat[2*i+1, 2*j]//4+mat[2*i+1, 2*j+1]//4)
     modify(matshrink)
 
 
@@ -134,51 +136,31 @@ def poster():
 
 def rotate():
     mat=load(nomImgCourante)
-    matrotate=np.empty((mat.shape[1],mat.shape[0],4),dtype=np.uint8)
+    matrotate = np.empty((mat.shape[1], mat.shape[0], 3), dtype = np.uint8)
     for i in range(mat.shape[0]):
         for j in range(mat.shape[1]):
-            matrotate[j,mat.shape[0]-i-1]=mat[i,j]
+            matrotate[j, mat.shape[0]-i-1] = mat[i, j]
     modify(matrotate)
 
 def luminosite():
     mat=load(nomImgCourante)
     for i in range(mat.shape[0]):
         for j in range(mat.shape[1]):
-            if(mat[i,j][0]+3*scale_lum.get()<256):
+            if (mat[i, j][0]+3*scale_lum.get()<245):
                 mat[i,j][0]=(mat[i,j][0]+3*scale_lum.get())
-            else :
+            else:
                 mat[i,j][0]=255
             if (mat[i,j][1]+8*scale_lum.get()<256):
                 mat[i,j][1]=(mat[i,j][1]+8*scale_lum.get())
             else:
                 mat[i,j][1]=255
-            if (mat[i,j][2]+scale_lum.get()<256):
+            if (mat[i,j][2]+scale_lum.get()<245):
                 mat[i,j][2]=(mat[i,j][2]+scale_lum.get())
-            else :
+            else:
                 mat[i,j][2]=255
     modify(mat)
 
-def bruit():
-    mat=load(nomImgCourante)
-    for i in range(mat.shape[0]):
-        for j in range(mat.shape[1]):
-            mat[i,j]+=randint(0,100)
-    modify(mat)
 
-def dithering():
-    mat=load(nomImgCourante)
-    for i in range(mat.shape[0]):
-        for j in range(mat.shape[1]):
-            seuil=randint(0,255)
-            gris=0.2125*mat[i,j,0]+0.7154*mat[i,j,1]+0.0721*mat[i,j,2]
-            if (seuil<gris):
-                mat[i,j]=(255,255,255,mat[i,j,3])
-            else :
-                mat[i,j]=[0,0,0,mat[i,j,3]]            
-    modify(mat)
-
-def Quit():
-    fenetre.quit()
 create=True
 #Fonctions auxiliaires 
     
@@ -187,39 +169,34 @@ fenetre.title("mon petit photoshop")
 
 
 #Création des Widgets
-Dithering=tk.Button(fenetre,text='dithering',command=dithering)
-Bruit=tk.Button(fenetre,text='bruit',command=bruit)
-Rotate=tk.Button(fenetre,text='rotate',command=rotate)
-Poster=tk.Button(fenetre,text='flou',command=poster)
-Shrink=tk.Button(fenetre,text='shrink',command=shrink)
 Zoom=tk.Button(fenetre,text="zoom",command=zoom)
 noirblanc=tk.Button(fenetre,text="Noir et blanc",command=noirBlanc)
 filtregris=tk.Button(fenetre,text="filtre gris",command=gris)
 filtrevert=tk.Button(fenetre,text="filtre vert",command=filtre_vert)
 retour=tk.Button(fenetre,text="Retour",command=reaffiche)
 Charger=tk.Button(fenetre,text="Charger", command=lambda:charger(fenetre))
-quitter=tk.Button(fenetre,text="Quitter", command=Quit)
-# label=tk.Label(fenetre,text="BENICHOU Lily-may 22200339")
-scale_lum=tk.Scale(fenetre,from_=0,to_=32)
-Luminosite=tk.Button(fenetre,text='luminosite',command=luminosite)
+quitter=tk.Button(fenetre,text="Quitter", command=arreter)
+label=tk.Label(fenetre,text="Emma CLUZET 22203903")
+Shrink = tk.Button(fenetre, text="Shrink", command= shrink)
+Flou = tk.Button(fenetre, text="Flou", command= poster)
+rotation = tk.Button(fenetre, text="Rotation", command= rotate)
+Luminosite = tk.Button(fenetre, text="luminosité", command=luminosite)
+scalelum=tk.Scale(fenetre, from_=0, to_=32)
+# Positionnement des Widgets
 
-#Positionnement des Widgets
-Dithering.grid(column=2,row=5)
-Bruit.grid(column=3,row=4)
-scale_lum.grid(column=4,row=2)
-Luminosite.grid(column=1,row=5)
-Rotate.grid(column=3,row=5)
-Poster.grid(column=0,row=5)
-Shrink.grid(column=0,row=3)
 Zoom.grid(column=3,row=0)
 noirblanc.grid(column=0,row=2)
 filtregris.grid(column=3,row=2)
 filtrevert.grid(column=3,row=3)
 retour.grid(column=0,row=0)
 Charger.grid(column=0,row=4)
-quitter.grid(column=4,row=4)
-#label.grid(column=2,row=4)
-
-
+quitter.grid(column=3,row=4)
+label.grid(column=2,row=4)
+Shrink.grid(column=0, row=3)
+Flou.grid(column=3, row=1)
+rotation.grid(column=0, row=1)
+Luminosite.grid(column=0, row=5)
+scalelum.grid(column=4, row=2)
 #Lancement de la boucle 
+
 fenetre.mainloop()
